75b18e9a5bd1049cd994e52b87a7aab105506ee0
Salomon1337 <salomonpl2000@gmail.com>
2022-06-18 20:08:24 +0200

    Add Friend and Message models

---
+++ Client/MVVM/Model/FriendModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.MVVM.Model
+{
+    internal class FriendModel
+    {   
+        public string Username { get; set; }
+        public string ImageSource { get; set; }
+    }
+}
