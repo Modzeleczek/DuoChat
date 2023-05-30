4042a64d11d4ca5285ebda7f04372f15a7d849c2
Mariusz Rostkowski <salomonpl2000@gmail.com>
2023-05-30 22:23:11 +0200

    Add second theme and functionality to change themes

--- Client/App.xaml.cs
+++ Client/App.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Client.MVVM.View.Windows;
 using Client.MVVM.ViewModel;
+using Client.Properties;
+using System;
 using System.Windows;
 
 namespace Client
@@ -10,5 +12,33 @@ namespace Client
         {
             new MainWindow(null, new MainViewModel()).Show();
         }
+
+        private ResourceDictionary ThemeDictionary => Resources.MergedDictionaries[0];
+
+        public App()
+        {
+            InitializeComponent();
+
+            switch (Settings.Default.CurrentTheme)
+            {
+                
+                case "Light":
+                    ChangeTheme(new Uri("/MVVM/View/Themes/LightTheme.xaml", UriKind.Relative));
+                    Settings.Default.CurrentTheme = "Light";
+                    Settings.Default.Save();
+                    break;
+                default:
+                    ChangeTheme(new Uri("/MVVM/View/Themes/DarkTheme.xaml", UriKind.Relative));
+                    Settings.Default.CurrentTheme = "Dark";
+                    Settings.Default.Save();
+                    break;
+            }
+        }
+        public void ChangeTheme(Uri uri)
+        {
+            ThemeDictionary.MergedDictionaries.Clear();
+            ThemeDictionary.MergedDictionaries.Add(new ResourceDictionary() { Source = uri });
+        }
+
     }
 }
