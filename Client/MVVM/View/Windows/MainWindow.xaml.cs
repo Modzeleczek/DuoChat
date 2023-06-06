9306c5af17fe29e01340ca1e0c4a970daef96ae0
Modzeleczek <lukasz.modzelewski1@gmail.com>
2023-06-07 00:19:04 +0200

    fix: Remove code previously moved to ResizableWindow

--- Client/MVVM/View/Windows/MainWindow.xaml.cs
+++ Client/MVVM/View/Windows/MainWindow.xaml.cs
@@ -1,12 +1,8 @@
 using Client.MVVM.ViewModel;
 using Shared.MVVM.Core;
 using Shared.MVVM.View.Windows;
-using System;
-using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Shapes;
 
 namespace Client.MVVM.View.Windows
 {
@@ -51,78 +47,6 @@ namespace Client.MVVM.View.Windows
             else
                 WindowState = WindowState.Normal;
         }
-
-        #region ResizeWindows
-        bool ResizeInProcess = false;
-        private void Resize_Init(object sender, MouseButtonEventArgs e)
-        {
-            Rectangle senderRect = sender as Rectangle;
-            if (senderRect != null)
-            {
-                ResizeInProcess = true;
-                senderRect.CaptureMouse();
-            }
-        }
-
-        private void Resize_End(object sender, MouseButtonEventArgs e)
-        {
-            Rectangle senderRect = sender as Rectangle;
-            if (senderRect != null)
-            {
-                ResizeInProcess = false; ;
-                senderRect.ReleaseMouseCapture();
-            }
-        }
-
-        private void Resizeing_Form(object sender, MouseEventArgs e)
-        {
-            if (ResizeInProcess)
-            {
-                double temp = 0;
-                Rectangle senderRect = sender as Rectangle;
-                Window mainWindow = senderRect.Tag as Window;
-                if (senderRect != null)
-                {
-                    double width = e.GetPosition(mainWindow).X;
-                    double height = e.GetPosition(mainWindow).Y;
-                    senderRect.CaptureMouse();
-                    if (senderRect.Name.Contains("right", StringComparison.OrdinalIgnoreCase))
-                    {
-                        width += 5;
-                        if (width > 0)
-                            mainWindow.Width = width;
-                    }
-                    if (senderRect.Name.Contains("left", StringComparison.OrdinalIgnoreCase))
-                    {
-                        width -= 5;
-                        temp = mainWindow.Width - width;
-                        if ((temp > mainWindow.MinWidth) && (temp < mainWindow.MaxWidth))
-                        {
-                            mainWindow.Width = temp;
-                            mainWindow.Left += width;
-                        }
-                    }
-                    if (senderRect.Name.Contains("bottom", StringComparison.OrdinalIgnoreCase))
-                    {
-                        height += 5;
-                        if (height > 0)
-                            mainWindow.Height = height;
-                    }
-                    if (senderRect.Name.ToLower().Contains("top", StringComparison.OrdinalIgnoreCase))
-                    {
-                        height -= 5;
-                        temp = mainWindow.Height - height;
-                        if ((temp > mainWindow.MinHeight) && (temp < mainWindow.MaxHeight))
-                        {
-                            mainWindow.Height = temp;
-                            mainWindow.Top += height;
-                        }
-                    }
-                }
-            }
-        }
-        
-        #endregion
 
         private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
