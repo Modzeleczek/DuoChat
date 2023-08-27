82b946c925c70491f8de84e61f9ee650ba73bb7e
Modzeleczek <lukasz.modzelewski1@gmail.com>
2023-08-27 02:55:42 +0200

    refac: Extract common style resources to Shared
    refac: In Server, replace hard-coded colors with references to DynamicResources
    
    refac: In Server, in styles refer to DynamicResources instead of StaticResources

--- Server/App.xaml.cs
+++ Server/App.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Server.MVVM.View.Windows;
 using Server.MVVM.ViewModel;
+using Shared.MVVM.Core;
+using System;
 using System.Windows;
 
 namespace Server
@@ -9,6 +11,34 @@ namespace Server
         protected override void OnStartup(StartupEventArgs e)
         {
             new MainWindow(null, new MainViewModel()).Show();
+        }
+
+        private ResourceDictionary ThemeDictionary => Resources.MergedDictionaries[0];
+
+        public enum Theme { Dark, Light }
+        // domyślnie ustawiamy ciemny motyw
+        private Theme _activeTheme = Theme.Dark;
+        public Theme ActiveTheme
+        {
+            get => _activeTheme;
+            set
+            {
+                var uri = $"/MVVM/View/Resources/Dynamic/Themes/{value}.xaml";
+                ThemeDictionary.MergedDictionaries.Clear();
+                ThemeDictionary.MergedDictionaries.Add(
+                    new ResourceDictionary() { Source = new Uri(uri, UriKind.Relative) });
+                _activeTheme = value;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        public void ToggleTheme()
+        {
+            ActiveTheme = ActiveTheme.Next();
         }
     }
 }
