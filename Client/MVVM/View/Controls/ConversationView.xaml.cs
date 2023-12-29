bd6c79a07f701a739a63c943b1fc8867e6062b8d
Modzeleczek <lukasz.modzelewski1@gmail.com>
2023-12-29 23:40:29 +0100

    fix: Keep message ScrollViewer position in place when inserting new messages at top

--- Client/MVVM/View/Controls/ConversationView.xaml.cs
+++ Client/MVVM/View/Controls/ConversationView.xaml.cs
@@ -7,6 +7,8 @@ namespace Client.MVVM.View.Controls
     {
         #region Fields
         private bool _autoScroll;
+        private bool _insertedElements = false;
+        private double _extentHeightBeforeInsert = 0;
         #endregion
 
         public ConversationView()
@@ -16,6 +18,16 @@ namespace Client.MVVM.View.Controls
 
         private void MessageScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
         {
+            double currentHeigth = MessageScrollViewer.ExtentHeight;
+            if (_insertedElements && currentHeigth > _extentHeightBeforeInsert)
+            {
+                /* Po dodaniu elementów na samej górze, następuje kilka wywołań ScrollChanged
+                i dopiero w którymś z nich ExtentHeight faktycznie się powiększa. */
+                MessageScrollViewer.ScrollToVerticalOffset(currentHeigth - _extentHeightBeforeInsert);
+                // Oznaczamy operację dodawania jako zakończoną.
+                _insertedElements = false;
+            }
+
             // https://stackoverflow.com/a/19315242
             // User scroll event : set or unset auto-scroll mode
             if (e.ExtentHeightChange == 0)
@@ -32,6 +44,10 @@ namespace Client.MVVM.View.Controls
 
                     if (MessageScrollViewer.VerticalOffset == 0)
                     {
+                        // Oznaczamy operację dodawania jako rozpoczętą.
+                        _insertedElements = true;
+                        _extentHeightBeforeInsert = MessageScrollViewer.ExtentHeight;
+
                         // Użytkownik ręcznie doscrollował na samą górę.
                         var getMoreMessagesCommand = ((ConversationViewModel)DataContext).GetMoreMessages;
                         if (getMoreMessagesCommand.CanExecute(null))
